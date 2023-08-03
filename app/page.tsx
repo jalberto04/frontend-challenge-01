9a324e6028ce0327cb965d2320c73b205482e559
Ciarán Curley <ciaran.curley.11.11@gmail.com>
2023-08-03 16:42:45 +0100

    feat placeholder components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-import { GameHeader } from "@/components/GameHeader";
+import GameBoard from "@/components/GameBoard";
+import GameHeader from "@/components/GameHeader";
+import GameStats from "@/components/GameStats";
 
 export default function Page() {
   return <>
     <GameHeader />
+    <main>
+        <GameBoard />
+        <GameStats />
+    </main>
   </>;
 }
--- components/GameHeader.tsx
+++ components/GameHeader.tsx
@@ -1,4 +1,4 @@
-export function GameHeader() {
+export default function GameHeader() {
   return (
     <header className="p-12 flex flex-col space-y-2 text-center">
       <h1 className="text-3xl font-bold tracking-tight mx-auto">TIC TAC TOE GAME</h1>

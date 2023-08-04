5cd7590dacb0b1d6f042e55b37d157b55de2f92a
Ciarán Curley <ciaran.curley.11.11@gmail.com>
2023-08-04 14:09:06 +0100

    fix: update headers

--- components/GameHeader.tsx
+++ components/GameHeader.tsx
@@ -1,7 +1,7 @@
 export default function GameHeader() {
   return (
     <header className="p-12 flex flex-col space-y-2 text-center">
-      <h1 className="text-3xl font-bold tracking-tight mx-auto">TIC TAC TOE GAME</h1>
+      <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl lg:text-extrabold mx-auto">TIC TAC TOE GAME</h1>
     </header>
   );
 }

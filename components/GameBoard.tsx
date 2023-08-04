19d53015b198d79459984a0e161efc01783060bc
Ciarán Curley <ciaran.curley.11.11@gmail.com>
2023-08-04 12:54:48 +0100

    fix: style changes

--- components/GameBoard.tsx
+++ components/GameBoard.tsx
@@ -1,11 +1,11 @@
 export default function GameBoard() {
   return (
-    <section className="mx-auto flex w-full justify-center gap-x-12">
+    <section className="mx-auto flex w-full justify-center gap-x-12 bg-gray-200 py-14">
       <div className="flex flex-col justify-center text-center">
         <span className="font-bold text-2xl">PLAYER 1</span>
         <span className="font-semibold text-5xl">2</span>
       </div>
-      <div className="p-20 border border-solid">
+      <div className="p-20 border border-solid border-black">
         <span className="text-2xl">GAME BOARD</span>
       </div>
       <div className="flex flex-col justify-center text-center">
--- components/GameStats.tsx
+++ components/GameStats.tsx
@@ -1,3 +1,9 @@
 export default function GameBoard() {
-  return <section>Game Stats</section>;
+  return (
+    <section className="p-12 flex flex-col space-y-2 text-center">
+      <h1 className="text-3xl font-bold tracking-tight mx-auto">
+        STATS
+      </h1>
+    </section>
+  );
 }

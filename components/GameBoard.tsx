8b83f88ca47f5b62711e0dfac2cfae9bce2df192
Ciarán Curley <ciaran.curley.11.11@gmail.com>
2023-08-04 15:07:07 +0100

    fix: game board draft

--- components/GameBoard.tsx
+++ components/GameBoard.tsx
@@ -1,3 +1,15 @@
+'use client';
+
+import GameGrid from "./GameGrid";
+
+const grid = [
+  ["X", "O", "O"],
+  ["O", "X", "O"],
+  ["O", "O", "X"],
+];
+
+const onCellClick = (row: number, col: number) => {};
+
 export default function GameBoard() {
   return (
     <section className="mx-auto flex w-full justify-center gap-x-12 bg-gray-200 py-14">
@@ -6,7 +18,7 @@ export default function GameBoard() {
         <span className="font-semibold text-5xl">2</span>
       </div>
       <div className="p-20 border border-solid border-black">
-        <span className="text-2xl">GAME BOARD</span>
+        <GameGrid grid={grid} onCellClick={onCellClick}/>
       </div>
       <div className="flex flex-col justify-center text-center">
         <span className="font-bold text-2xl">PLAYER 2</span>
